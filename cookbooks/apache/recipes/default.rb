52cc57f54bfaa64f4319879838e48dfcdd2761ed
roei <roei@yomama.com>
2018-01-09 21:13:54 +0000

    added deb vs rhel condition and tested ohai attribute querying.

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,13 +5,22 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 #
 #
+
+#node attributes in this case from ohai tool
+if node['platform_family'] == 'rhel'
+	package = "httpd"
+elsif node['plaform_family'] == "debian"
+	package = "apache2"
+end
+
 package 'apache2' do
 #im trsting default behavior so no action 
 #default action is install
-	package_name 'httpd'
+	package_name package
 end
 
 service 'apache2' do
 	service_name 'httpd'
 	action [:start, :enable]
 end
+
